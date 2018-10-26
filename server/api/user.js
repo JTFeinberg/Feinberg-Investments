c69cb15ac602af3df9b27eadb4b33bc965fdf112
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-26 18:30:08 -0400

    feat(user) Add api post route for transactions

---
+++ server/api/user.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const {User, Transaction} = require('../db/models')
+module.exports = router
+
+router.post('/transaction', async (req, res, next) => {
+  const {action, stockSymbol, numOfShares, price, userId} = req.body
+  if (req.user.id === userId) {
+    try {
+      const newTransaction = await Transaction.create({
+        action,
+        stockSymbol,
+        numOfShares,
+        price
+      })
+      const user = await User.findById(userId)
+      newTransaction.setUser(user)
+      res.json(newTransaction)
+    } catch (err) {
+      next(err)
+    }
+  } else {
+    res.status(403).send('Unauthorized User')
+  }
+})
