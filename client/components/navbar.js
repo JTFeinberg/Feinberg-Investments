e1a835a1cb17bd73362fc249c34bd7b797fa87f9
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-26 13:15:53 -0400

    update(navbar) Add link to transactions in navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,12 +6,13 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Feinberg Investments</h1>
     <nav>
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
+          <Link to="/user/transaction_history">Transactions</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
