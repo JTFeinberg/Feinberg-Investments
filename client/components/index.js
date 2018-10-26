6ce0d8f4d2bd6c7c0a4976bc850aab0ddd82468a
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-26 17:22:42 -0400

    update(routes) Add TradeForm route for signed in users

--- client/components/index.js
+++ client/components/index.js
@@ -5,5 +5,6 @@
  */
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
-export {default as Transactions} from './transactions'
 export {Login, Signup} from './auth-form'
+export {default as Transactions} from './transactions'
+export {default as TradeForm} from './tradeForm'
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Transactions} from './components'
+import {Login, Signup, UserHome, Transactions, TradeForm} from './components'
 import {me} from './store'
 
 /**
@@ -26,6 +26,7 @@ class Routes extends Component {
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
             <Route path="/user/transaction_history" component={Transactions} />
+            <Route path="/user/trade_form" component={TradeForm} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
