c35a74df075a4e7c54e03e881293caead85d7a23
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-26 18:03:56 -0400

    update(tradeForm) Control form with handleChange function

--- client/components/tradeForm.js
+++ client/components/tradeForm.js
@@ -2,31 +2,50 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 class TradeForm extends Component {
- constructor(props){
-     super(props)
-     this.state = {
-        symbolInput: "",
-        sharesInput: "",
-      };
- }
+  constructor(props) {
+    super(props)
+    this.state = {
+      symbolInput: '',
+      sharesInput: '',
+      action: 'BUY'
+    }
+  }
 
+  handleChange = ({target}) => {
+      let {name, value} = target;
+      this.setState({[name]: value})
+  }
 
- render() {
-     let {allsymbols, user} = this.props
-     return (
-       <div>
-           <h1>Cash on Hand {user.balance}</h1>
-       <form>
-           <input name="symbolInput" maxLength="6" placeholder="Enter Stock Symbol Here"/>
-           <input name="shares" type="number" maxLength="10" placeholder="Qty of Shares"/>
-           <select name="action">
-               <option value="BUY">BUY</option>
-               <option value="SELL">SELL</option>
-           </select>
-       </form>
-       </div>
-     )
- }
+  render() {
+    let {allsymbols, user} = this.props
+    let {symbolInput, sharesInput, action} = this.state
+    return (
+      <div>
+        <h1>Cash on Hand ${user.balance}</h1>
+        <form>
+          <input
+            name="symbolInput"
+            value={symbolInput}
+            onChange={this.handleChange}
+            maxLength="6"
+            placeholder="Enter Stock Symbol Here"
+          />
+          <input
+            name="sharesInput"
+            value={sharesInput}
+            onChange={this.handleChange}
+            maxLength="10"
+            placeholder="Qty of Shares"
+          />
+          <select name="action" value={action} onChange={this.handleChange}>
+            <option value="BUY">BUY</option>
+            <option value="SELL">SELL</option>
+          </select>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
+    )
+  }
 }
 
 /**
@@ -38,6 +57,5 @@ const mapState = state => {
     user: state.user
   }
 }
-
 
 export default connect(mapState)(TradeForm)
