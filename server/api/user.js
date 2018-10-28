fc3b94b9e4d1ee82f8cab1aec5b1ee86979f2dac
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-28 14:22:20 -0400

    update(transaction) Add backend validation to transaction route

--- server/api/user.js
+++ server/api/user.js
@@ -1,20 +1,28 @@
 const router = require('express').Router()
 const {User, Transaction} = require('../db/models')
+const IEX_API = 'https://api.iextrading.com/1.0'
+const axios = require('axios')
+
 module.exports = router
 
 router.post('/transaction', async (req, res, next) => {
   const {action, stockSymbol, numOfShares, price, userId} = req.body
   if (req.user.id === userId) {
     try {
-      const newTransaction = await Transaction.create({
-        action,
-        stockSymbol,
-        numOfShares,
-        price
-      })
       const user = await User.findById(userId)
-      newTransaction.setUser(user)
-      res.json(newTransaction)
+      const stockInfo = await axios.get(`${IEX_API}/stock/market/batch?symbols=${stockSymbol}&types=quote`)
+      if(user.balance >= numOfShares * price && stockInfo.data[stockSymbol] && !numOfShares.includes('.')){
+        const newTransaction = await Transaction.create({
+          action,
+          stockSymbol,
+          numOfShares,
+          price
+        })
+        newTransaction.setUser(user)
+        res.json(newTransaction)
+      } else {
+        res.status(403).send('Invalid Transaction')
+      }
     } catch (err) {
       next(err)
     }
