b0a37c80edbf4c01f5cf10a93b99bc010bf27f20
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-28 21:06:43 -0400

    style(transactions) Bold and center header

--- client/components/transactions.js
+++ client/components/transactions.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-
 /**
  * COMPONENT
  */
@@ -9,32 +8,32 @@ const Transactions = ({transactions}) => {
   return transactions.length ? (
     <div className="transactions-container">
       <h3>Transaction History</h3>
-      <ul>
-                <li>Action</li>
-                <li>Stock Symbol</li>
-                <li>Number of Shares</li>
-                <li>Price Per Share</li>
-                <li>Toal Value</li>
-                <li>Date of Purchase</li>
-            </ul>
+      <ul className="transactions-header">
+        <li>Action</li>
+        <li>Stock Symbol</li>
+        <li>Number of Shares</li>
+        <li>Price Per Share</li>
+        <li>Toal Value</li>
+        <li>Date of Purchase</li>
+      </ul>
       {transactions.map(currTrans => {
-         return (
-            <ul key={currTrans.id}>
-                <li>{currTrans.action}</li>
-                <li>{currTrans.stockSymbol}</li>
-                <li>{`${currTrans.numOfShares} shares`}</li>
-                <li>{`@ $${currTrans.price}`}</li>
-                <li>{`$${currTrans.value}`}</li>
-                <li>{`${currTrans.createdAt.split('T')[0]}`}</li>
-            </ul>
-          )
+        return (
+          <ul key={currTrans.id}>
+            <li>{currTrans.action}</li>
+            <li>{currTrans.stockSymbol}</li>
+            <li>{`${currTrans.numOfShares} shares`}</li>
+            <li>{`@ $${currTrans.price}`}</li>
+            <li>{`$${currTrans.value}`}</li>
+            <li>{`${currTrans.createdAt.split('T')[0]}`}</li>
+          </ul>
+        )
       })}
     </div>
-  ) : ( 
-  <div>
-    <h3>You have no transaction histrory</h3>
-    <h3>Click here to begin trading!</h3>
-  </div>
+  ) : (
+    <div>
+      <h3>You have no transaction histrory</h3>
+      <h3>Click here to begin trading!</h3>
+    </div>
   )
 }
 
