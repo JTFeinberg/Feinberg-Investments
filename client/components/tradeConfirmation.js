248f799395ea95a0d2d6242ccb5bf721249e38f4
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-30 19:55:19 -0400

    update(tradeConfirmaiton) Fetch transaction if none is on state

--- client/components/tradeConfirmation.js
+++ client/components/tradeConfirmation.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchTradedStockThunk} from '../store'
 
 /**
  * COMPONENT
  */
-export const TradeConfirmation = ({transaction}) => {
-
-  return (
+export const TradeConfirmation = ({match, transaction, loadTransactionData}) => {
+    if (!transaction.id) loadTransactionData(match.params.id)
+  return transaction.id ? (
     <div>
      {`${transaction.id}`}
     </div>
+  ) : (
+      <div>
+          <h1>LOADING...</h1>
+      </div>
   )
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapStateToProps = state => {
   return {
     transaction: state.trade
   }
 }
 
-export default connect(mapState)(TradeConfirmation)
+const mapDispatchToProps = dispatch => {
+    return {
+      loadTransactionData(id) {
+          dispatch(fetchTradedStockThunk(id))
+      }
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(TradeConfirmation)
 
