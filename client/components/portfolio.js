fc7397ad893d382c8f6605343da6b1577a93c490
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-30 10:51:38 -0400

    update(portfolio) Change component to class

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {IEX_API} from '../'
 import {fetchPortfolioThunk} from '../store'
@@ -7,48 +7,50 @@ import axios from 'axios'
 /**
  * COMPONENT
  */
-const Portfolio = ({stocks}) => {
-
-    const stockInfo = async (stockSymbol) => {
-        if (stocks) {
-            let stockSymbolsStr = stocks.map(currStock => currStock.stockSymbol).join(',')
-            console.log('STOCKSYMBOLS', stockSymbolsStr)
-            let currInfo = await axios.get(`${IEX_API}/stock/market/batch?symbols=${stockSymbolsStr}&types=quote`)
-            console.log(currInfo.data[stockSymbol].quote.latestPrice)
-            return currInfo.data[stockSymbol].quote
-        }
-    }
-
-  return stocks && stocks.length ? (
-    <div className="transactions-container">
-      <h3>Transaction History</h3>
-      <ul className="transactions-header">
-        <li>Stock Symbol</li>
-        <li>latest Price</li>
-        <li>Number of Shares</li>
-        <li>Price Per Share</li>
-        <li>Toal Value</li>
-        <li>Date of Purchase</li>
-      </ul>
-      {stocks.map((currStock, idx) => {
-        return (
-          <ul className={`transactions-row ${idx % 2 === 0 ? 'shade-alternate': '' }`} key={currStock.id} >
-            <li>{currStock.stockSymbol}</li>
-            <li>{`$${stockInfo(currStock.stockSymbol).latestPrice}`}</li>
-            <li>{`${currStock.numOfShares} shares`}</li>
-            <li>{`$${Number(currStock.costBasis).toFixed(2)}/share`}</li>
-            <li>{`$${Number(currStock.totalInvested).toFixed(2)}`}</li>
-            <li>{`${currStock.createdAt.split('T')[0]}`}</li>
+class Portfolio extends Component {
+    // comp
+    // const stockInfo = async (stockSymbol) => {
+    //     if (stocks) {
+    //         let stockSymbolsStr = stocks.map(currStock => currStock.stockSymbol).join(',')
+    //         console.log('STOCKSYMBOLS', stockSymbolsStr)
+    //         let currInfo = await axios.get(`${IEX_API}/stock/market/batch?symbols=${stockSymbolsStr}&types=quote`)
+    //         console.log(currInfo.data[stockSymbol].quote.latestPrice)
+    //         return currInfo.data[stockSymbol].quote
+    //     }
+    // }
+  render() {
+      const {stocks, latestStockData} = this.props
+      return stocks && stocks.length ? (
+        <div className="transactions-container">
+          <h3>Transaction History</h3>
+          <ul className="transactions-header">
+            <li>Stock Symbol</li>
+            <li>latest Price</li>
+            <li>Number of Shares</li>
+            <li>Price Per Share</li>
+            <li>Toal Value</li>
+            <li>Date of Purchase</li>
           </ul>
-        )
-      })}
-    </div>
-  ) : (
-    <div>
-      <h3>You have no transaction histrory</h3>
-      <h3>Click here to begin trading!</h3>
-    </div>
-  )
+          {stocks.map((currStock, idx) => {
+            return (
+              <ul className={`transactions-row ${idx % 2 === 0 ? 'shade-alternate': '' }`} key={currStock.id} >
+                <li>{currStock.stockSymbol}</li>
+                <li>{`$${stockInfo(currStock.stockSymbol).latestPrice}`}</li>
+                <li>{`${currStock.numOfShares} shares`}</li>
+                <li>{`$${Number(currStock.costBasis).toFixed(2)}/share`}</li>
+                <li>{`$${Number(currStock.totalInvested).toFixed(2)}`}</li>
+                <li>{`${currStock.createdAt.split('T')[0]}`}</li>
+              </ul>
+            )
+          })}
+        </div>
+      ) : (
+        <div>
+          <h3>You have no transaction histrory</h3>
+          <h3>Click here to begin trading!</h3>
+        </div>
+      )
+  }
 }
 
 /**
@@ -56,7 +58,8 @@ const Portfolio = ({stocks}) => {
  */
 const mapStateToProps = state => {
   return {
-    stocks: state.user.portfolios
+    stocks: state.user.portfolios,
+    latestStockData: state.portfolio
   }
 }
 
