8c7cc917214069d87d887aae194e903a9307248a
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-31 12:41:12 -0400

    update(singleStock) Load current stock data

--- client/components/singleStock.js
+++ client/components/singleStock.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {fetchPortfolioThunk} from '../store'
 
 /**
  * COMPONENT
  */
-const SingleStock = props => {
-
-  return (
+const SingleStock = ({stock, match, loadStockData}) => {
+    let {stockSymbol} = match.params
+    loadStockData(stockSymbol)
+  return stock[stockSymbol] ? (
     <div>
-      <h1>Welcome to the single Stock Page!</h1>
+      <h1>Welcome to the {stock[stockSymbol].quote.symbol} Page!</h1>
     </div>
-  )
+  ) : null
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapStateToProps = state => {
   return {
-    stock: state.trade
+    stock: state.portfolio
   }
 }
 
-export default connect(mapState)(SingleStock)
+const mapDispatchToProps = dispatch => {
+    return {
+      loadStockData(stockSymbol) {
+          dispatch(fetchPortfolioThunk(stockSymbol))
+      }
+    }
+  }
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleStock)
