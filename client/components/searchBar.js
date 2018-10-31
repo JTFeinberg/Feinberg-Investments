06f8de042c317def5ffe3dcf8ce3423740cf0fef
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-31 17:52:54 -0400

    update(searchBar) handleChange of inputs

--- client/components/searchBar.js
+++ client/components/searchBar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 /**
  * COMPONENT
@@ -11,12 +12,21 @@ class SearchBar extends Component {
           stockSymbol: ''
       }
   }
+  handleChange = ({target}) => {
+    let {name, value} = target
+    this.setState({[name]: value.toUpperCase()})
+  }
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    this.props.history.push(`/stock_info/${evt.target.value}`)
+  }
 render() {
+    const {stockSymbol} = this.state
     return (
       <form id="search-bar">
-          <input name="stockSymbol" maxLength="6"
+          <input name="stockSymbol" value={stockSymbol} onChange={this.handleChange} maxLength="6"
             placeholder="Search Stock Symbols"/>
-          <input type="submit" id="search-submit"/>
+          <Link to={`/stock_info/${stockSymbol}`}>LINK</Link>
       </form>
     )
 }
