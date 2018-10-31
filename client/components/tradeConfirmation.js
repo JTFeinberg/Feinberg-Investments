57c58e72d1273c867600484733209e3e076accae
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-30 20:25:53 -0400

    update(tradeConfirmaiton) Add in rows for data fields

--- client/components/tradeConfirmation.js
+++ client/components/tradeConfirmation.js
@@ -6,15 +6,34 @@ import {fetchTradedStockThunk} from '../store'
  * COMPONENT
  */
 export const TradeConfirmation = ({match, transaction, loadTransactionData}) => {
-    if (!transaction.id) loadTransactionData(match.params.id)
+  if (!transaction.id) loadTransactionData(match.params.id)
   return transaction.id ? (
-    <div>
-     {`${transaction.id}`}
+    <div className="transactions-container">
+      <h3>Transaction Confirmation</h3>
+      <div style={{display: "flex", justifyContent: "center"}}>
+
+      <ul>
+        <li>Action</li>
+        <li>Stock Symbol</li>
+        <li>Number of Shares</li>
+        <li>Price Per Share</li>
+        <li>Toal Value</li>
+        <li>Date of Purchase</li>
+      </ul>
+      <ul>
+        <li>{transaction.action}</li>
+        <li>{transaction.stockSymbol}</li>
+        <li>{`${transaction.numOfShares} shares`}</li>
+        <li>{`$${transaction.price}/share`}</li>
+        <li>{`$${Number(transaction.value).toFixed(2)}`}</li>
+        <li>{`${transaction.createdAt.split('T')[0]}`}</li>
+      </ul>
+      </div>
     </div>
   ) : (
-      <div>
-          <h1>LOADING...</h1>
-      </div>
+    <div>
+      <h1>LOADING...</h1>
+    </div>
   )
 }
 
@@ -28,12 +47,11 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => {
-    return {
-      loadTransactionData(id) {
-          dispatch(fetchTradedStockThunk(id))
-      }
+  return {
+    loadTransactionData(id) {
+      dispatch(fetchTradedStockThunk(id))
     }
   }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(TradeConfirmation)
-
