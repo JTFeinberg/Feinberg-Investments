37ed415d427b36f3cc5035b42125886d07fcea49
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-10-31 15:32:12 -0400

    update(singleStock) Add fields for data

--- client/components/singleStock.js
+++ client/components/singleStock.js
@@ -17,13 +17,28 @@ class SingleStock extends Component {
   
   render() {
     const {stock, match} = this.props
+    const {quote, logo} = stock
     //If stock symbol doesn't exist, let user know
     return !stock.error ? (
         //If stock does exist but old data is still on state, show Loading...
         //Otherwise show requested stock data
       stock.quote.symbol === match.params.stockSymbol.toUpperCase() ? (
         <div>
-          <h1>Welcome to the {stock.quote.symbol} Page!</h1>
+          <div className="stock-header">
+            <h1>{`(${quote.symbol}) ${quote.companyName}`}</h1>
+            <ul className="stock-header-list">
+              <li>{quote.sector}</li>
+            </ul>
+          </div>
+
+          <ul className='stock-left'>
+          <li><p>${quote.latestPrice}</p></li>
+          <li className={quote.latestPrice > quote.previousClose ? 'gain' : 'loss'}><p>${(quote.latestPrice - quote.previousClose).toFixed(2)} ({((quote.latestPrice - quote.previousClose)/quote.previousClose).toFixed(2)}%)</p></li>
+          </ul>
+          <ul className='stock-right'>
+          <li><p><span>Stock Symbol: </span>{quote.symbol}</p></li>
+          <li><p><span>Stock Symbol: </span>{quote.symbol}</p></li>
+          </ul>
         </div>
       ) : (
         <div>LOADING...</div>
