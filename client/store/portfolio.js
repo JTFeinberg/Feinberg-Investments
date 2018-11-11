34f728f9ad5e6ca02151ea3b5c44ba3fc485779c
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-11 16:34:52 -0500

    update(portfolio) Add REMOVE_PORTFOLIO action to store for logging out

--- client/store/portfolio.js
+++ client/store/portfolio.js
@@ -6,12 +6,14 @@ import {IEX_API} from '../'
  */
 
 const GET_PORTFOLIO = 'GET_PORTFOLIO'
+const REMOVE_PORTFOLIO = 'REMOVE_PORTFOLIO'
 
 
 /**
  * ACTION CREATORS
  */
 const getPortfolio = portfolio => ({type: GET_PORTFOLIO, payload: portfolio})
+const removePortfolio = () => ({type: REMOVE_PORTFOLIO, payload: {}})
 
 /**
  * THUNK CREATORS
@@ -26,13 +28,17 @@ export const fetchPortfolioThunk = (stockSymbols) => async dispatch => {
         console.error(err)
     }
 }
-
+export const removePortfolioThunk = () => dispatch => {
+  dispatch(removePortfolio())
+}
 /**
  * REDUCER
  */
 export default function (state = {}, action) {
   switch (action.type) {
     case GET_PORTFOLIO:
+      return action.payload
+    case REMOVE_PORTFOLIO:
       return action.payload
     default:
       return state
