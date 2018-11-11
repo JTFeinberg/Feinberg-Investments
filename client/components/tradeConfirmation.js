5ab265f374199f3222278a4d7aaf59caeace06c8
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-11 15:11:41 -0500

    update(tradeConfirmation) Convert function component to class

--- client/components/tradeConfirmation.js
+++ client/components/tradeConfirmation.js
@@ -1,42 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchTradedStockThunk, me} from '../store'
 
 /**
  * COMPONENT
  */
-export const TradeConfirmation = ({match, transaction, loadTransactionData}) => {
-  //If for some reason a user saves this url and they go back here, we need to load the transaction onto the state.
-  //Otherwise just use what was saved from when the user clicked submit on the trade form. 
-  if (!transaction.id) loadTransactionData(match.params.id)
-  return transaction.id ? (
-    <div className="trade-confirmation-container">
-      <h3>Transaction Confirmation</h3>
-      <div className="trade-confirmation-wrapper">
+class TradeConfirmation extends Component {
 
-      <ul className="trade-confirmation-col">
-        <li className="shade-alternate">Action</li>
-        <li>Stock Symbol</li>
-        <li className="shade-alternate">Number of Shares</li>
-        <li>Price Per Share</li>
-        <li className="shade-alternate">Toal Value</li>
-        <li>Date of Purchase</li>
-      </ul>
-      <ul className="trade-confirmation-col">
-        <li className="shade-alternate">{transaction.action}</li>
-        <li>{transaction.stockSymbol}</li>
-        <li className="shade-alternate">{`${transaction.numOfShares} shares`}</li>
-        <li>{`$${transaction.price}/share`}</li>
-        <li className="shade-alternate">{`$${Number(transaction.value).toFixed(2)}`}</li>
-        <li>{`${transaction.createdAt.split('T')[0]}`}</li>
-      </ul>
+  render() {
+    
+    //If for some reason a user saves this url and they go back here, we need to load the transaction onto the state.
+    //Otherwise just use what was saved from when the user clicked submit on the trade form. 
+    const {match, transaction, loadTransactionData} = this.props
+    if (!transaction.id) loadTransactionData(match.params.id)
+    return transaction.id ? (
+      <div className="trade-confirmation-container">
+        <h3>Transaction Confirmation</h3>
+        <div className="trade-confirmation-wrapper">
+  
+        <ul className="trade-confirmation-col">
+          <li className="shade-alternate">Action</li>
+          <li>Stock Symbol</li>
+          <li className="shade-alternate">Number of Shares</li>
+          <li>Price Per Share</li>
+          <li className="shade-alternate">Toal Value</li>
+          <li>Date of Purchase</li>
+        </ul>
+        <ul className="trade-confirmation-col">
+          <li className="shade-alternate">{transaction.action}</li>
+          <li>{transaction.stockSymbol}</li>
+          <li className="shade-alternate">{`${transaction.numOfShares} shares`}</li>
+          <li>{`$${transaction.price}/share`}</li>
+          <li className="shade-alternate">{`$${Number(transaction.value).toFixed(2)}`}</li>
+          <li>{`${transaction.createdAt.split('T')[0]}`}</li>
+        </ul>
+        </div>
       </div>
-    </div>
-  ) : (
-    <div>
-      <h1>LOADING...</h1>
-    </div>
-  )
+    ) : (
+      <div>
+        <h1>LOADING...</h1>
+      </div>
+    )
+  }
 }
 
 /**
