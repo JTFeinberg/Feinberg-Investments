6088d4f3ab8ab76a7ba88a932f82fc27a6528e93
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-20 15:43:00 -0500

    chore(searchBar) Change all instances of stockSymbol to searchString

--- client/components/searchBar.js
+++ client/components/searchBar.js
@@ -11,55 +11,55 @@ class SearchBar extends Component {
   constructor(props) {
       super(props)
       this.state = {
-          stockSymbol: ''
+          searchString: ''
       }
   }
   handleChange = ({target}) => {
-    this.setState({stockSymbol: target.value.toUpperCase()})
+    this.setState({searchString: target.value.toUpperCase()})
   }
   handleClick= (input) => {
     //This loads the data for the stock symbol the user is searching
     this.props.loadStockData(input)
     //After they click the search button we remove the input from the state/form
-    this.setState({stockSymbol: ''})
+    this.setState({searchString: ''})
   }
 render() {
-    const {stockSymbol} = this.state
+    const {searchString} = this.state
     const {stocksMap, symbolsArr} = this.props
-    const regex = new RegExp(stockSymbol, 'gi')
+    const regex = new RegExp(searchString, 'gi')
     //Match stocks on either their symbol or the company name
-    const filteredStocks = symbolsArr.filter(symbol => symbol.match(stockSymbol) || stocksMap.get(symbol).match(regex))
+    const filteredStocks = symbolsArr.filter(symbol => symbol.match(searchString) || stocksMap.get(symbol).match(regex))
     return (
       <div>
         <form id="search-bar" >
-            <input name="stockSymbol" value={stockSymbol} onChange={this.handleChange}
+            <input name="stockSymbol" value={searchString} onChange={this.handleChange}
               placeholder="Search Stock Symbols"/>
               {/* Go to the singleStock component for the searched stock symbol when the user clicks search button */}
-            <Link to={`/stock_info/${stockSymbol}`}><button id="search-bar-btn" onClick={() => this.handleClick(stockSymbol)}/></Link>
+            <Link to={`/stock_info/${searchString}`}><button id="search-bar-btn" onClick={() => this.handleClick(searchString)}/></Link>
         </form>
-        {stockSymbol.length > 1 ?
+        {searchString.length > 1 ?
         (<ul id="search-bar-list">
           <li className="list-header">Quotes</li>
           {filteredStocks.map(symbol => { 
             const companyName = stocksMap.get(symbol)
-            const indexOfSymbol = symbol.indexOf(stockSymbol)
+            const indexOfSymbol = symbol.indexOf(searchString)
             // Because the search string is always uppercase we need to change the case of the company name to find a match
-            const indexOfCompany = companyName.toUpperCase().indexOf(stockSymbol)
+            const indexOfCompany = companyName.toUpperCase().indexOf(searchString)
             return (
               <Link key={symbol} to={`/stock_info/${symbol}`}>
                 <li className="search-bar-list-item" onClick={() => this.handleClick(symbol)}>
                 {/* If the search input is found in the stock symbol, then make it bold. Otherewise just render the symbol. */}
                   {indexOfSymbol > -1 ? 
                     <span className="list-symbol">{symbol.slice(0, indexOfSymbol)}
-                      <strong>{stockSymbol}</strong>
-                    {symbol.slice(indexOfSymbol + stockSymbol.length)}</span> 
+                      <strong>{searchString}</strong>
+                    {symbol.slice(indexOfSymbol + searchString.length)}</span> 
                     :
                     <span className="list-symbol">{symbol}</span>}
                 {/* If the search input is found in the company name, then make it bold. Otherewise just render the name. */}
                   {indexOfCompany > -1 ? 
                     <span className="list-name">{companyName.slice(0, indexOfCompany)}
-                      <strong>{companyName.slice(indexOfCompany, stockSymbol.length)}</strong>
-                    {companyName.slice(indexOfCompany + stockSymbol.length)}</span> 
+                      <strong>{companyName.slice(indexOfCompany, searchString.length)}</strong>
+                    {companyName.slice(indexOfCompany + searchString.length)}</span> 
                     :
                     <span className="list-name">{companyName}</span>}
                 </li>
@@ -85,8 +85,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-      loadStockData(stockSymbol) {
-        dispatch(fetchStockThunk(stockSymbol))
+      loadStockData(searchString) {
+        dispatch(fetchStockThunk(searchString))
       }
     }
   }
