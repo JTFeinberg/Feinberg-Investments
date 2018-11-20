4858b9abc49b0622d76f9f8278182df6e4b62cf3
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-11-20 15:39:50 -0500

    chore(searchBar) Add comments to explain changes

--- client/components/searchBar.js
+++ client/components/searchBar.js
@@ -27,6 +27,7 @@ render() {
     const {stockSymbol} = this.state
     const {stocksMap, symbolsArr} = this.props
     const regex = new RegExp(stockSymbol, 'gi')
+    //Match stocks on either their symbol or the company name
     const filteredStocks = symbolsArr.filter(symbol => symbol.match(stockSymbol) || stocksMap.get(symbol).match(regex))
     return (
       <div>
@@ -42,16 +43,19 @@ render() {
           {filteredStocks.map(symbol => { 
             const companyName = stocksMap.get(symbol)
             const indexOfSymbol = symbol.indexOf(stockSymbol)
+            // Because the search string is always uppercase we need to change the case of the company name to find a match
             const indexOfCompany = companyName.toUpperCase().indexOf(stockSymbol)
             return (
               <Link key={symbol} to={`/stock_info/${symbol}`}>
                 <li className="search-bar-list-item" onClick={() => this.handleClick(symbol)}>
+                {/* If the search input is found in the stock symbol, then make it bold. Otherewise just render the symbol. */}
                   {indexOfSymbol > -1 ? 
                     <span className="list-symbol">{symbol.slice(0, indexOfSymbol)}
                       <strong>{stockSymbol}</strong>
                     {symbol.slice(indexOfSymbol + stockSymbol.length)}</span> 
                     :
                     <span className="list-symbol">{symbol}</span>}
+                {/* If the search input is found in the company name, then make it bold. Otherewise just render the name. */}
                   {indexOfCompany > -1 ? 
                     <span className="list-name">{companyName.slice(0, indexOfCompany)}
                       <strong>{companyName.slice(indexOfCompany, stockSymbol.length)}</strong>
